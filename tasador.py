bfafc624f82262407a381a5c710810eb5400777b
victorcasascanada2-beep <victorcasascanada2@gmail.com>
2026-01-13 18:21:26 +0100

    Update tasador.py

--- tasador.py
+++ tasador.py
@@ -1,2 +1,15 @@
 import streamlit as st
-import VersionEstable1_0
+import os
+
+# 1. Nombre del archivo que quieres arrancar (CON el .py esta vez)
+VERSION_ACTIVA = "VersionEstable1_0.py" 
+
+def lanzar_version(archivo):
+    if os.path.exists(archivo):
+        # Este comando lee el archivo y lo ejecuta directamente
+        exec(open(archivo, encoding="utf-8").read(), globals())
+    else:
+        st.error(f"No encuentro el archivo {archivo} en GitHub")
+
+# Ejecutamos
+lanzar_version(VERSION_ACTIVA)
