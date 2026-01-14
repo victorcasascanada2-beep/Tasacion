22f169e6c528bfee6a75e0e6e24e80a62787c0db
victorcasascanada2-beep <victorcasascanada2@gmail.com>
2026-01-14 17:44:15 +0100

    Update tasador.py

--- tasador.py
+++ tasador.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 # Asegúrate de que el nombre sea exacto y sin espacios
-VERSION = "VersionEstable1_0.py"
+VERSION = "VersionExperta2_0.py"
 
 if os.path.exists(VERSION):
     with open(VERSION, "r", encoding="utf-8") as f:
